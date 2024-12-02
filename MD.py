f52baefe4189ff244ba1b467212684f7fb0211b6
Nick Tiwari <nicktiwari@Nicks-MacBook-Pro-5.local>
2024-12-02 12:22:55 -0500

    Fixed matplotlib

--- MD.py
+++ MD.py
@@ -1,3 +1,4 @@
+from numpy._typing import _256Bit
 from pyomo.environ import value
 from flowsheets import MD_single_stage_continuous_recirculation as MD
 import matplotlib.pyplot as plt
@@ -8,20 +9,20 @@ import time
 import sys
 
 area = 100
-
-#Build the model
+plt.rcParams.update({'font.size': 14})
 
 # Create a plot with 3 columns and 1 row
 fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 
-membrane_coeff = np.arange(15,25,2)
-recovery = np.arange(0.4,0.8,0.1)
+membrane_coeff = np.arange(5,10,1)
+recovery = np.arange(0.4,0.8,0.2)
 
 color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 for rec, color in zip(recovery, color_cycle):
     lcow = []
     sec = []
+    membrane_area = []
     for num in membrane_coeff:
         m = MD.build()
         MD.set_operating_conditions(m, recovery=rec, recycle_coeff=num)
@@ -29,14 +30,25 @@ for rec, color in zip(recovery, color_cycle):
         MD.solve(m)
         lcow.append(value(m.fs.costing.LCOW))
         sec.append(value(m.fs.costing.specific_energy_consumption))
-        m.fs.costing.LCOW.display()
+        membrane_area.append(m.fs.MD.area.value)
 
     # Plot index vs lcow in the first plot
-    ax[0].scatter(membrane_coeff, lcow, color=color)
-    ax[1].scatter(membrane_coeff, sec, color=color)
-    ax[2].scatter(sec, lcow)
+    ax[0].scatter(membrane_area, lcow, color=color, label='Fractional recovery: {:.1f}'.format(rec))
+    ax[0].set_ylabel('Levelized Cost of Water (\$/m$^3$ water)')
+    ax[0].set_xlabel('Membrane Area (m$^2$)')
+    ax[1].scatter(membrane_area, sec, color=color, label='Fractional recovery: {:.1f}'.format(rec))
+    ax[1].set_ylabel('Specific Energy Consumption (kWh/m$^3$ water)')
+    ax[1].set_xlabel('Membrane Area (m$^2$)')
+    ax[2].scatter(sec, lcow, label='Fractional recovery: {:.1f}'.format(rec))
+    ax[2].set_xlabel('Specific Energy Consumption (kWh/m$^3$ water)')
+    ax[2].set_ylabel('Levelized Cost of Water (\$/m$^3$ water)')
 
+ax[0].legend()
+ax[1].legend()
+ax[2].legend()
+plt.savefig('seclcow.png', dpi=300)
 plt.show()
+
 
 
 # Get feed volumetric flowrate
