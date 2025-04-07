1f7b026c33c075cf348db1269636c45f2925fecf
Adam Atia <aatia@keylogic.com>
2025-04-07 14:45:11 -0400

    update oaro to use multi stage flowsheet and generate results for 50% recovery from NF permeate conditions

--- examples/oaro/oaro.py
+++ examples/oaro/oaro.py
@@ -1,4 +1,4 @@
-from watertap.flowsheets.oaro import oaro
+from watertap.flowsheets.oaro import oaro_multi as oaro
 from idaes.core.util.model_statistics import degrees_of_freedom
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,42 +15,25 @@ from watertap.core.util.model_diagnostics import infeasible as infeas
 # Motivation: determine why increased feed flowrates lead to failure to converge (solves at 1 kg/s, fails at 5 kg/s)
 # Takeaway: Membrane areas should be adjusted with flowrate, and some vars should be unfixed for optimization.
 
+# 4/7/2025: replacing OARO flowsheet with multi-stage OARO flowsheet
 
 class ERDtype(StrEnum):
     pump_as_turbine = "pump_as_turbine"
 
-def main(erd_type=ERDtype.pump_as_turbine, raise_on_failure=False):
-    # set up solver
-    solver = get_solver()
+if __name__ == "__main__":
+    solver= get_solver()
+    m = oaro.main(number_of_stages=4, system_recovery=0.5, erd_type=ERDtype.pump_as_turbine)
+    
+    
+     # Removing upper bounds on OARO module dimensions, but more importantly, unfixing OARO module area!
+    for stage in m.fs.NonFinalStages:
+        m.fs.OAROUnits[stage].area.unfix()
+        m.fs.OAROUnits[stage].area.setub(None)
+        m.fs.OAROUnits[stage].length.setub(None)
+        m.fs.OAROUnits[stage].width.setub(None)
 
-    # build, set, and initialize
-    m = oaro.build(erd_type=erd_type)
-    oaro.set_operating_conditions(m)
-
-    feed_flow_mass = 1 # kg/s
-    feed_mass_frac_NaCl = 0.03
-    feed_mass_frac_H2O = 1 - feed_mass_frac_NaCl
-    m.fs.feed.properties[0].flow_mass_phase_comp["Liq", "NaCl"].fix(
-        feed_flow_mass * feed_mass_frac_NaCl
-    )
-    m.fs.feed.properties[0].flow_mass_phase_comp["Liq", "H2O"].fix(
-        feed_flow_mass * feed_mass_frac_H2O
-    )
-
-    oaro.initialize_system(m, solver=solver)
-    oaro.optimize_set_up(m)
-
-    # For optimization, consider fixing system recovery rate. Will set to 50%, though low for current settings/application, consistent with MD recovery setting.
-    m.fs.volumetric_recovery.fix(0.5)
-
-    # Removing upper bounds on OARO module dimensions, but more importantly, unfixing OARO module area!
-    m.fs.OARO.area.unfix()
-    m.fs.OARO.area.setub(None)
-    m.fs.OARO.length.setub(None)
-    m.fs.OARO.width.setub(None)
-
-    # Unfix OARO feed velocity, which would otherwise remained fixed at 10 cm/s
-    m.fs.OARO.feed_side.velocity[0,0].unfix()
+        # Unfix OARO feed velocity
+        m.fs.OAROUnits[stage].feed_side.velocity[0,0].unfix()
 
     # Removing upper bounds on RO module dimensions, but more importantly, unfixing RO module width!
     m.fs.RO.width.unfix()
@@ -58,58 +41,71 @@ def main(erd_type=ERDtype.pump_as_turbine, raise_on_failure=False):
     m.fs.RO.width.setub(None)
     m.fs.RO.length.setub(None)
 
-    res = oaro.solve(m, solver=solver)
+    
+
+    m.fs.feed.flow_mass_phase_comp[0,"Liq","H2O"].fix(7.2258)
+    m.fs.feed.flow_mass_phase_comp[0,"Liq","NaCl"].fix(0.64491)
+    res = solver.solve(m, tee=True)
     assert_optimal_termination(res)
 
-    print("\n***---Optimization results---***")
-    oaro.display_system(m)
-    if erd_type == ERDtype.pump_as_turbine:
-        oaro.display_state(m)
-    else:
-        pass
+    m.fs.mass_water_recovery.unfix()
+    m.fs.water_recovery.fix(0.5)
+    res = solver.solve(m, tee=True)
+    assert_optimal_termination(res)
+    # m = main()
+    # feed_mass_frac_NaCl = 0.03
+    # feed_mass_frac_H2O = 1 - feed_mass_frac_NaCl
 
-    return m
+    # # Removing upper bounds on OARO module dimensions, but more importantly, unfixing OARO module area!
+    # m.fs.OARO.area.unfix()
+    # m.fs.OARO.area.setub(None)
+    # m.fs.OARO.length.setub(None)
+    # m.fs.OARO.width.setub(None)
 
-if __name__ == "__main__":
-    m = main()
-    feed_mass_frac_NaCl = 0.03
-    feed_mass_frac_H2O = 1 - feed_mass_frac_NaCl
+    # # Unfix OARO feed velocity
+    # m.fs.OARO.feed_side.velocity[0,0].unfix()
 
-    # Removing upper bounds on OARO module dimensions, but more importantly, unfixing OARO module area!
-    m.fs.OARO.area.unfix()
-    m.fs.OARO.area.setub(None)
-    m.fs.OARO.length.setub(None)
-    m.fs.OARO.width.setub(None)
+    # # Removing upper bounds on RO module dimensions, but more importantly, unfixing RO module width!
+    # m.fs.RO.width.unfix()
+    # m.fs.RO.area.setub(None)
+    # m.fs.RO.width.setub(None)
+    # m.fs.RO.length.setub(None)
 
-    # Unfix OARO feed velocity
-    m.fs.OARO.feed_side.velocity[0,0].unfix()
+    # feed_flow_mass = 1 # kg/s
+    # m.fs.feed.properties[0].flow_mass_phase_comp["Liq", "NaCl"].fix(
+    #     feed_flow_mass * feed_mass_frac_NaCl
+    # )
+    # feed_mass_frac_H2O = 1 - feed_mass_frac_NaCl
+    # m.fs.feed.properties[0].flow_mass_phase_comp["Liq", "H2O"].fix(
+    #     feed_flow_mass * feed_mass_frac_H2O
+    # )
+    # res = oaro.solve(m, tee=True)
 
-    # Removing upper bounds on RO module dimensions, but more importantly, unfixing RO module width!
-    m.fs.RO.width.unfix()
-    m.fs.RO.area.setub(None)
-    m.fs.RO.width.setub(None)
-    m.fs.RO.length.setub(None)
 
-
+    # # NF Permeate conditions
+    # m.fs.feed.flow_mass_phase_comp[0,"Liq","H2O"].fix(7.2258)
+    # m.fs.feed.flow_mass_phase_comp[0,"Liq","NaCl"].fix(0.64491)
+    # res = oaro.solve(m, tee=False)
+    # assert_optimal_termination(res)
     # Let's loop through mass flowrates, from 2 to 5 kg/s. 5 kg/s can solve now.
-    for i in range(2,6):
-        feed_flow_mass = i # kg/s
-        m.fs.feed.properties[0].flow_mass_phase_comp["Liq", "NaCl"].fix(
-            feed_flow_mass * feed_mass_frac_NaCl
-        )
-        feed_mass_frac_H2O = 1 - feed_mass_frac_NaCl
-        m.fs.feed.properties[0].flow_mass_phase_comp["Liq", "H2O"].fix(
-            feed_flow_mass * feed_mass_frac_H2O
-        )
-        res = oaro.solve(m, tee=True)
-        print(f"FLOWRATE = {i}")
+    # for i in range(2,6):
+    #     feed_flow_mass = i # kg/s
+    #     m.fs.feed.properties[0].flow_mass_phase_comp["Liq", "NaCl"].fix(
+    #         feed_flow_mass * feed_mass_frac_NaCl
+    #     )
+    #     feed_mass_frac_H2O = 1 - feed_mass_frac_NaCl
+    #     m.fs.feed.properties[0].flow_mass_phase_comp["Liq", "H2O"].fix(
+    #         feed_flow_mass * feed_mass_frac_H2O
+    #     )
+    #     res = oaro.solve(m, tee=True)
+    #     print(f"FLOWRATE = {i}")
 
-        if check_optimal_termination(res):
-            oaro.display_system(m)
+    #     if check_optimal_termination(res):
+    #         oaro.display_system(m)
 
-            m.fs.OARO.area.display()
-            m.fs.RO.area.display()
+    #         m.fs.OARO.area.display()
+    #         m.fs.RO.area.display()
 
-        else:
-            print("SOLVE FAILED")
-            infeas.print_infeasible_constraints(m)
+    #     else:
+    #         print("SOLVE FAILED")
+    #         infeas.print_infeasible_constraints(m)
