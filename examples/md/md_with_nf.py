587494b5f25fab99f54031c85168c91e9ab43233
Chad Able <chad.able@netl.doe.gov>
2025-04-24 12:08:59 -0400

    added to the with NF sim

--- examples/md/md_with_nf.py
+++ examples/md/md_with_nf.py
@@ -86,8 +86,13 @@ if __name__ == "__main__":
     m.fs.overall_recovery.display()
 
     # NF Permeate conditions
-    m.fs.feed.flow_mass_phase_comp[0,"Liq","H2O"].fix(7.2258)
-    m.fs.feed.flow_mass_phase_comp[0,"Liq","TDS"].fix(0.64491)
+    # m.fs.feed.flow_mass_phase_comp[0,"Liq","H2O"].fix(7.2258)
+    # m.fs.feed.flow_mass_phase_comp[0,"Liq","TDS"].fix(0.64491)
+    m.fs.feed.flow_mass_phase_comp[0, "Liq", "H2O"].unfix()
+    m.fs.feed.flow_mass_phase_comp[0, "Liq", "TDS"].unfix()
+    m.fs.feed.properties[0].flow_vol_phase["Liq"].fix(0.007439)                # volumetric flow rate (m3/s), equal to 235.8 gpm/2 post NF
+    m.fs.feed.properties[0].conc_mass_phase_comp["Liq", "TDS"].fix(86.650)        # conc in g/L #base 86.650 post NF at 50%
+
     res = MD.solve(m, tee=False)
     if check_optimal_termination(res):
         # m.fs.MD.report()
